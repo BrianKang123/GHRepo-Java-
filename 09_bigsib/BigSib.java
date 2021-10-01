e8ed139883a82a05459c7b6aa29b0364f2d007e9
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-01 12:24:16 -0400

    Create BigSib.java

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,17 @@
+Brian Kang
+APCS
+HW07: Saving your Class
+2021-10-1
+
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
+    }
+} 
+  
+public static void main(String[] args) {
+System.out.println("Why, hello there, Roo. How are you feeling today?");
+    }
+}  
