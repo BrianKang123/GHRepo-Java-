e940aa936dde74719cad834d8279fe9d8bbef5fa
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 02:12:53 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,1 +1,12 @@
-
+Brian Kang and Vivian (Duckie)
+APCS
+HW10: Refactor Big Sib One
+2021-10-4
+  
+  public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
