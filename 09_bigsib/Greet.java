6df39a4a4276f4de8a92d8e0aed9bc6acc5e61da
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 12:37:15 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,7 +1,7 @@
-Brian Kang and Vivian (Duckie)
-APCS
-HW09: What Are BigSibs Good For?
-2021-10-1
+//Brian Kang and Vivian (Duckie)
+//APCS
+//HW09: What Are BigSibs Good For?
+//2021-10-1
 
 public class Greet {
     public static void main( String[] args ) {
