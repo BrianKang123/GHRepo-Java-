7d3545cf012d476cb21c60ff176e7f35dfe1dca8
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 12:25:19 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,8 +1,9 @@
-Brian Kang and Vivian (Duckie)
-APCS
-HW10: Refactor Big Sib One
-2021-10-4
+//Brian Kang and Vivian (Duckie)
+//APCS
+//HW10: Refactor Big Sib One
+//2021-10-4
 
+public
 public static void main(String[] args) {
 System.out.println("Why, hello there, " ". How are you feeling today?");
     }
