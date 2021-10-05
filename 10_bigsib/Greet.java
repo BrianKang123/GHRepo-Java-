3ad578028bee355431fdf7fec75c65114e07d37d
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 12:26:03 -0400

    Update Greet.java

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -3,8 +3,10 @@
 //HW10: Refactor Big Sib One
 //2021-10-4
 
-public
-public static void main(String[] args) {
-System.out.println("Why, hello there, " ". How are you feeling today?");
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
     }
 }
