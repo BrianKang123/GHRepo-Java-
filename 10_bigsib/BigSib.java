26303c085e231b9a7108b39a24588a5506e48145
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 12:28:13 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,16 +1,15 @@
-Brian Kang and Vivian (Duckie)
-APCS
-HW10: Refactor Big Sib One
-2021-10-4
+//Brian Kang and Vivian (Duckie)
+//APCS
+//HW10: Refactor Big Sib One
+//2021-10-4
   
- Discoveries: You can directly make changes in Github if the SSH terminal has issues or whatever you are more comfortable with. 
+/* Discoveries: You can directly make changes in Github if the SSH terminal has issues or whatever you are more comfortable with. 
 Unresolved Questions: Am I doing the assignment correctly?
   How can I make the BigSib java file run the class Greet?
-  
-  public class Greet {
-    public static void main( String[] args ){
-        System.out.println( BigSib.greet("Crosby") );
-        System.out.println( BigSib.greet("Stills") );
-        System.out.println( BigSib.greet("Nash") );
-    }
-}
+*/
+
+  public class BigSib {
+      public static String greet(String name) {
+        return "Why, hello there, " + name + ". How do you do?" ;
+      }
+  }
