74b02179d1e7f4a23c78f761b3edc88cb8f46ca3
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 12:34:06 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -3,7 +3,10 @@ APCS
 HW09: What Are BigSibs Good For?
 2021-10-1
 
-public static void main(String[] args) {
-System.out.println("Why, hello there, Roo. How are you feeling today?");
+public class Greet {
+    public static void main( String[] args ) {
+        BigSib.greet("Flim");
+        BigSib.greet("Flam");
+        BigSib.greet("Kazaam");
     }
 } 
