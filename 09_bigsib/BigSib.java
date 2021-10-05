62f33e468c8b0e59e41fae6c8dd18e0819787665
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 12:32:19 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,7 +1,7 @@
-Brian Kang and Vivian (Duckie)
-APCS
-HW09: What Are BigSibs Good For?
-2021-10-1
+//Brian Kang and Vivian (Duckie)
+//APCS
+//HW09: What Are BigSibs Good For?
+//2021-10-1
 
 public class Greet {
     public static void main( String[] args ) {
@@ -11,7 +11,4 @@ public class Greet {
     }
 } 
   
-public static void main(String[] args) {
-System.out.println("Why, hello there, Roo. How are you feeling today?");
-    }
-}  
+ 
