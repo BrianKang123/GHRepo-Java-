5422c100718635b8f9776d61826408dae4be7da0
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 02:18:14 -0400

    Update BigSib.java

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -3,6 +3,10 @@ APCS
 HW10: Refactor Big Sib One
 2021-10-4
   
+ Discoveries: You can directly make changes in Github if the SSH terminal has issues or whatever you are more comfortable with. 
+Unresolved Questions: Am I doing the assignment correctly?
+  How can I make the BigSib java file run the class Greet?
+  
   public class Greet {
     public static void main( String[] args ){
         System.out.println( BigSib.greet("Crosby") );
