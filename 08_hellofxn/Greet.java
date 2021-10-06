b3d0fea25eaa7227e82d2c02489f6e9164838a10
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 22:10:07 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,4 +1,16 @@
-public static void main(String[] args) {
-System.out.println("Why, hello there, Roo. How are you feeling today?");
+// Brian Kang
+// APCS
+// HW08 -- Refactor Freshie Zero
+// 2021-09-30
+
+public class Greet {
+    public static void main(String[] args) {
+        greet("Gordon Ramsay");
+        greet("George Washington");
+        greet("Abraham Lincoln");
+    }
+    
+    public static void greet(String name) {
+    	System.out.println("Why, hello there, " + name + ". How do you do?");
     }
 }
