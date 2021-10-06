289c9d690c2514e3da22b3d3a2b6867ef7e7aaf8
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-05 22:06:31 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,7 +1,16 @@
-// Brian Kang and Ryan Lau
+// TNPG: Orion(Brian Kang and Ryan Lau)
 // APCS
 // HW12 -- On Elder Individuality and the Elimination of Radio Fuzz
 // 2021-10-05
+
+/* 
+DISCOVERIES
+
+UNRESOLVED QUESTIONS
+1. Why does the prepoerty helloMsg need to be static in BigSib.java?
+2. If a property doesn't have a public or private signature, then what signature does it have?
+
+*/
 
 public class BigSib {
     static String helloMsg;
