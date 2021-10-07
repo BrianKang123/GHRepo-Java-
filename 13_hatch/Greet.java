40d1fa3d4b1e708870d6d86caa32985114aaaff8
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-07 04:52:51 -0400

    Create Greet.java

---
+++ 13_hatch/Greet.java
@@ -0,0 +1,33 @@
+// TNPG: Orion (Ryan Lau, Brian Kang, Jon, and Vivian)
+// APCS
+// HW13 -- Where do BigSibs Come From?
+// 2021-10-06
+
+/*
+DISCOVERIES
+UNRESOLVED QUESTIONS
+    0. How can we provide arguments to the declaration of a instance of a class?
+*/
+
+public class Greet {
+    public static void main(String[] args) {
+        String greeting;
+
+        BigSib richard = new BigSib();
+
+        greeting = richard.greet("freshman");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Salutations");
+        greeting = richard.greet("Dr. Spaceman");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Hey ya");
+        greeting = richard.greet("Kong Fooey");
+        System.out.println(greeting);
+
+        richard.setHelloMsg("Sup");
+        greeting = richard.greet("mom");
+        System.out.println(greeting);
+    }
+}
