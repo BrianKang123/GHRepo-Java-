6f6b4ded7069681ced485546ed3b96a39b80e908
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-14 12:24:05 -0400

    Update BankAccount.java

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -7,8 +7,10 @@ public class BankAccount {
   private String fullName = "";
   
   public static void main(String[] args) {
-    setFullName("test name");
+    BankAccount 1 = new BankAccount();
+    1.setFullName("test name");
   }
+  
   
   public String setFullName(String newFullName) {
     fullName = newFullName;
