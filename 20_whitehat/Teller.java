ea494f468293e7b70a17d04fe26209f9017d94f3
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-19 22:19:46 -0400

    Update Teller.java

--- 20_whitehat/Teller.java
+++ 20_whitehat/Teller.java
@@ -1,127 +1,60 @@
 /**
-   Team NBJ, Jun Hong Wang, Brian K., Nada Hameed
-   APCS pd6
-   HW19 -- Mo Money Mo Problems ...MORE AWESOME
-   2021-10-18
-   **/
+  Team NBJ, Jun Hong Wang, Brian K., Nada Hameed
+  APCS pd6
+  HW20 -- External Audit
+  2021-10-19
+  
+  Reviewed by: NBJ
+  Reviewed team: Holister
+  
+  DISCO:
+  I don't think this is intended, but setting the account balance, and depositing into the account can be done regardless of if authenicate works or not. Not only that, but I'm not sure if their withdraw and deposit methods work as intended.
+  In the first bank account, we were able to subtract money, even when our credentials were false.
+  Upon further inspection, authenticate doesn't seem to do anything other than verifying that the account number and password are valid. 
+  Also, their pin number isn't actually used. It's just information that gets inputted, stored, and returned, but we aren't asked for the pin anywhere.
+  It doesn't prevent us from interacting with the account even if the credentials are wrong, since all it does it return a true or false value that does nothing.
+  We also went through their code, and the pins 1000 and 9998 aren't valid, when they should be.
+  
+  What Teller.java does it setup the BankAccount with its information, and from there we can deposit/withdraw money. 
+  It's pretty much the main method from BankAccount.java, but moved over to another file. 
+  
+  QCC:
+  A common issue between BankAccounts is that we don't account for depositing negative numbers, and this pretty much allows us to steal from accounts. 
+  In the first account, we were able to empty the bank account using this.
+  
+  What operations are possible from BankAccount's main method but not Teller's?
+  Initially, authenticate() was a private method, only accessible from BankAccount.java, but we changed it to public so it could be accessed here.
+  This might be the only thing that can only be accessed from BankAccount but not Teller.
+  Whatever methods are set to private are inaccessable from Teller, and the old authenticate method falls under this.
+**/
 
-public class Teller{
-
-  //instance variables
-  private String name;
-  private String passwd;
-  private short pin;
-  private int acctNum;
-  private double balance;
-  //---------------------------------------------
-
-
-  //mutators
-  public String setName( String newName ) {
-    String oldName = name;
-    name = newName;
-    return oldName;
-  }
-
-  public String setPasswd( String newPasswd ) {
-    String oldPasswd = passwd;
-    passwd = newPasswd;
-    return oldPasswd;
-  }
-
-  public short setPin( short newPin ) {
-    short oldPin = pin;
-    if ((newPin > 1000) && (newPin < 9998)) {
-      pin = newPin;
-      return oldPin;
-    } else {
-      pin = 9999;
-      System.err.println("New pin is not within range [1000, 9998].");
-      return oldPin;
-    }
-  }
-
-  public int setAcctNum( int newAcctNum ) {
-    int oldAcctNum = acctNum;
-    if ((newAcctNum >= 100000000) && (newAcctNum <= 999999998)) {
-      acctNum = newAcctNum;
-      return oldAcctNum;
-    } else {
-      acctNum = 999999999;
-      System.err.println("New account number is not within the range [100000000, 999999998].");
-      return oldAcctNum;
-    }
-  }
-
-  public double setBalance( double newBalance ) {
-    double oldBalance = balance;
-    balance = newBalance;
-    return oldBalance;
-  }
-  //---------------------------------------------
-
-
-  public void deposit( double depositAmount ) {
-    balance = balance + depositAmount;
-  }
-
-  public void withdraw( double withdrawAmount ) {
-    if (withdrawAmount > balance) {
-      System.err.println(false + " - account doesn't have enough money.");
-    } else {
-      balance = balance - withdrawAmount;
-      System.out.println (true + " - withdrawal success");
-  }
-}
-
-  //overwritten toString()
-  public String toString() {
-    String retStr = "\nAccount info:\n=======================";
-    retStr = retStr + "\nName: " + name;
-    retStr = retStr + "\nPassword: " + passwd;
-    retStr = retStr + "\nPIN: " + pin;
-    retStr = retStr + "\nAccount Number: " + acctNum;
-    retStr = retStr + "\nBalance: " + balance;
-    retStr = retStr + "\n=======================";
-    return retStr;
-  }
-
-private boolean authenticate( int accNum, String pass ) {
-  if ((accNum == acctNum) && (pass == passwd)) {
-    return (true);
-  } else {
-    return (false);
-  }
-}
-
-  //main method for testing
+public class Teller {
   public static void main( String[] args ) {
-    Teller ba = new Teller();
-      System.out.println(ba.toString());
-  //prints errors
-    
+    BankAccount ba = new BankAccount();
+    System.out.println(ba.toString());
+  
+    // using wrong credentials but still accessing bank account details, also stealing money using negative deposit.
     short badPin = 0100;
     ba.setName("badPepe");
     ba.setPasswd("PepeIsBad");
     ba.setPin(badPin);
     ba.setAcctNum(000000001);
+    ba.setBalance(100000000);
+    ba.deposit (-200000000);
+    ba.withdraw(0);
+    System.out.println("Authentication " + ba.authenticate(0000000001, "PepeIsBad"));
     System.out.println(ba.toString());
-    ba.setBalance(1000.00);
-    ba.deposit (200.00);
-    ba.withdraw(10000.99);
-    System.out.println("Authentication " + ba.authenticate(123456789, "PepeIsGod"));
     
-//prints full info
-  short pin = 1234;
+    // using the deposit method to deposit negative amount, but with right credentials
+    short pin = 1234;
     ba.setName("Pepe");
     ba.setPasswd("PepeIsGod");
     ba.setPin(pin);
     ba.setAcctNum(123456789);
-    System.out.println(ba.toString());
-    ba.setBalance(10000000.00);
-    ba.deposit(-200000.00);
-    ba.withdraw(200.00);
+    ba.setBalance(20000000.00);
+    ba.deposit(-1000000000);
+    ba.withdraw(0);
     System.out.println("Authentication " + ba.authenticate(123456789, "PepeIsGod"));
-}//end main()
-
-}//end class BankAccount
+    System.out.println(ba.toString());
+  }
+}
