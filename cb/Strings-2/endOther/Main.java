5c583c29d7c115a6e8df444d5d91bc5cb8b94412
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-31 21:03:50 -0400

    Update Main.java

--- cb/Strings-2/endOther/Main.java
+++ cb/Strings-2/endOther/Main.java
@@ -1,1 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(endOther("Hiabc", "abc")); // true
+        System.out.println(endOther("AbC", "HiaBc")); // true
+        System.out.println(endOther("abc", "abXabc")); // true
+    }
 
+public static boolean endOther(String a, String b) {
+      a = a.toLowerCase();
+      b = b.toLowerCase();
+
+      if (a.length() > b.length()){
+        return a.substring(a.length()-b.length()).equals(b);
+      }
+      else{
+        return b.substring(b.length()-a.length()).equals(a);
+      }
+    }
+}
