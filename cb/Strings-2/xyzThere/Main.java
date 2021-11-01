911f06a12bb65f65668a3251920ffc30cc001b7b
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-31 21:04:20 -0400

    Update Main.java

--- cb/Strings-2/xyzThere/Main.java
+++ cb/Strings-2/xyzThere/Main.java
@@ -1,1 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(xyzThere("abcxyz")); // true
+        System.out.println(xyzThere("abc.xyz")); // false
+        System.out.println(xyzThere("xyz.abc")); // true
+    }
+  
+public static boolean xyzThere(String str) {
+      boolean result = false;
+      for (int i = 0; i <= str.length()-3; i++){
+        if (str.substring(i, i+3).equals("xyz")&& (i == 0 || !(str.substring(i-1,i).equals(".")))){
+          result = true;
+        }
 
+      }
+      return result;
+    }
+}
