29f55c90325be655564181c904e6244da96aeb52
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-31 21:01:36 -0400

    Update Main.java

--- cb/Strings-2/countHi/Main.java
+++ cb/Strings-2/countHi/Main.java
@@ -1,1 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(countHi("abc hi ho")); // 1
+        System.out.println(countHi("ABChi hi")); // 2
+        System.out.println(countHi("hihi")); // 2
 
+  public static int countHi(String str) {
+        int counter = 0;
+        for (int i = 0; i <= str.length()-2; i++){
+          //System.out.println(str.substring(i, i+2));
+          if (str.substring(i, i+2).equals("hi")){
+            //System.out.println("match");
+            counter += 1;
+          }
+        }
+        return counter;
+    }
+}
