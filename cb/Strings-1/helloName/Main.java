0a4166eabc3785d16ddf9c0d660723f7b2327151
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-31 20:45:21 -0400

    Update Main.java

--- cb/Strings-1/helloName/Main.java
+++ cb/Strings-1/helloName/Main.java
@@ -1,1 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(helloName("Bob")); // "Hello Bob!"
+        System.out.println(helloName("Alice")); // "Hello Alice!"
+        System.out.println(helloName("X")); // "Hello X!"
+    }
 
+  public static String helloName(String name) {
+        return "Hello " + name + "!";
+    }
+}
