15d1eb1eb51643e7f4c153955fafbca9bcd6b186
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-31 21:02:56 -0400

    Update Main.java

--- cb/Strings-2/countCode/Main.java
+++ cb/Strings-2/countCode/Main.java
@@ -1,1 +1,17 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(countCode("aaacodebbb")); // 1
+        System.out.println(countCode("codexxcode")); // 2
+        System.out.println(countCode("cozexxcope")); // 2
+    }
+  
+public static int countCode(String str) {
+      int counter = 0;
+      for (int i = 0; i <= str.length()-4; i++){
+        if (str.substring(i, i+2).equals("co")&& str.substring(i+3, i+4).equals("e")){
+          counter += 1;
+        }
+      }
+      return counter;
+    }
+}
