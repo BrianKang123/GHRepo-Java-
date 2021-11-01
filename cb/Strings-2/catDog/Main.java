a79555aad1443fd1b4040b9bc820ebe5611a21e3
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-31 21:02:16 -0400

    Update Main.java

--- cb/Strings-2/catDog/Main.java
+++ cb/Strings-2/catDog/Main.java
@@ -1,1 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(catDog("catdog")); // true
+        System.out.println(catDog("catcat")); // false
+        System.out.println(catDog("1cat1cadodog")); // true
+    }
 
+ public static boolean catDog(String str) {
+        int catCount = 0;
+        int dogCount = 0;
+        for (int i =0; i <= str.length()-3; i++){
+          if (str.substring(i, i+3).equals("cat")){
+            catCount ++;
+          }
+          if (str.substring(i, i+3).equals("dog")){
+            dogCount ++;
+          }
+        }
+        return catCount == dogCount;
+    }
+}
