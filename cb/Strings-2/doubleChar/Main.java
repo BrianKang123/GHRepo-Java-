2edf60190c7093beca965392e17147ecf30223b1
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2021-10-31 21:00:35 -0400

    Update Main.java

--- cb/Strings-2/doubleChar/Main.java
+++ cb/Strings-2/doubleChar/Main.java
@@ -1,1 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(doubleChar("The")); // "TThhee"
+        System.out.println(doubleChar("AAbb")); // "AAAAbbbb"
+        System.out.println(doubleChar("Hi-There")); // "HHii--TThheerree"
+    }
+  
+ public static String doubleChar(String str) {
+      String result = "";
+      for (int i = 1; i <= str.length(); i++){
+        result += str.substring(i-1, i) + str.substring(i-1, i) ;
+      }
+      return result;
 
+    }
+}
