1d66b507d4d9e80879acfcc44e70c79ec5a5fc38
Brian <bkang30@stuy.edu>
2021-11-12 12:32:15 -0500

    add loopier

--- 34_array/Loopier.java
+++ 34_array/Loopier.java
@@ -6,16 +6,41 @@
 
 
 public class Loopier{
-  public static int[] fillArray(int[] arr){
-    for(int i = 0 ; i < arr.length ; i += 1){
-      arr[i] = (int)(Math.random() * 100); 
-    }
-    return arr;
-  }
- public static void main(String[] arghmemateys){
-    int[] argh = new int[10]; 
-    fillArray(argh);
-    System.out.println(argh.toString());
- }
+	public static int[] fillArray(int[] arr){
+		for(int i = 0 ; i < arr.length ; i += 1){
+			arr[i] = (int)(Math.random() *100); 
+		}
+		return arr;
+	}
+
+	public static String toString(int[] arr) {
+		String res = "[";
+		for (int element: arr) {
+			res = res + element + ", ";
+		}
+		res = res.substring(0, res.length() - 2);
+		res += "]";
+
+		return res;
+	}
+	
+	public static int linSearchI(int[] arr, int target){
+		for(int i = 0 ; i < arr.length ; i+=1){
+			if(arr[i] == target){
+				return i;
+			}
+		}
+		return -1;
+	}
+	// public static int linSearchR(int[] arr, int target){
+		
+	
+	// }
+	public static void main(String[] arghmemateys){
+		int[] argh = new int[10]; 
+		fillArray(argh);
+		System.out.println(toString(argh));
+		System.out.println(linSearchI(argh, 69));
+	}
 }
 
