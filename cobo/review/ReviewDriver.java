a0e70901bdaff2165b134a636bbc139446f85f22
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2022-02-11 04:24:14 -0500

    Update ReviewDriver.java

--- cobo/review/ReviewDriver.java
+++ cobo/review/ReviewDriver.java
@@ -1,9 +1,9 @@
 public class ReviewDriver {
     public static void main(String[] args) {
-        System.out.println(Review.sentimentVal("Hello"));
-        System.out.println(Review.sentimentVal("Thinkeren"));
-        System.out.println(Review.sentimentVal("Brian"));
-        System.out.println(Review.sentimentVal("Money"));
+        System.out.println(Review.sentimentVal("hello"));
+        System.out.println(Review.sentimentVal("thinkeren"));
+        System.out.println(Review.sentimentVal("brian"));
+        System.out.println(Review.sentimentVal("money"));
 
 
         System.out.println(Review.totalSentiment("SimpleReview.txt"));
