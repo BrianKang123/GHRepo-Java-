70983769eae1eeb66af91505d1d31844acb30114
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2022-02-15 00:08:30 -0500

    Update bunnyEars2.java

--- cb/recursion-1/bunnyEars2.java
+++ cb/recursion-1/bunnyEars2.java
@@ -14,12 +14,15 @@ public int bunnyEars2(int bunnies) {
 }
 
   public static void main(String[] args) {
-    System.out.print(bunnyEars2(0) + " → \"0");
-    System.out.print(bunnyEars2(1) + " → \"2");
-    System.out.print(bunnyEars2(2) + " → \"5");
+    System.out.print(bunnyEars2("0") + " → \"0");
+    System.out.print(bunnyEars2("1") + " → \"2");
+    System.out.print(bunnyEars2("2") + " → \"5");
 
   }
 }
+
+
+//We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
 
 //Not tree recursion
 //Only one line of calls and returns
