fdacf51b3edb1f0d5cc0307cad2fdaa881a7f0fa
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2022-02-15 00:17:07 -0500

    Update count7.java

--- cb/recursion-1/count7.java
+++ cb/recursion-1/count7.java
@@ -10,6 +10,16 @@ public int count7(int n) {
   return 0;
 }
 
+  public static void main(String[] args){
+    System.out.println(count7("717") + " → 2");
+    System.out.println(count7("7") + " → 1");
+    System.out.println(count7("123") + " → 0");
+  }
+
+//Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+
+
 //not tree recursion
 //single line of calls and returns
 //number shrinking smaller and smaller as the trace progresses
