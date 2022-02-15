88b3508fcae833344ee3d7527add1494a4c19704
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2022-02-15 00:19:40 -0500

    Update count8.java

--- cb/recursion-1/count8.java
+++ cb/recursion-1/count8.java
@@ -14,6 +14,14 @@ public int count8(int n) {
   return 0;
 }
 
+  public static void main(String[] args){
+    System.out.println(count8("8") + " → 1");
+    System.out.println(count8("818") + " → 2");
+    System.out.println(count8("8818") + " → 4");
+  }
+
+//Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit, except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4. Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
 //Not tree recursion
 //line of calls, only variation is one line of ifs
 //Number cut by units place each time, also checking for doubles
