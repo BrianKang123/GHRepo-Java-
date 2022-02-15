48c05c0a205d5e75dd0198d04ee5f4f18da55327
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2022-02-15 00:13:53 -0500

    Update sumDigits.java

--- cb/recursion-1/sumDigits.java
+++ cb/recursion-1/sumDigits.java
@@ -5,6 +5,14 @@ public int sumDigits(int n) {
   return 0;
 }
 
+  public static void main(String[] args){
+    System.out.println(countX(126") + " → 9");
+    System.out.println(countX("49") + " → 13");
+    System.out.println(countX("12") + " → 3");
+  }
+
+//Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
 //Not tree recursion
 //Only one line of calls and returns
 //A straight list of calls and return values
