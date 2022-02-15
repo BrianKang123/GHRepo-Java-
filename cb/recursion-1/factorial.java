4b7771e303026e420fc805cebd0c9262b47080ae
Brian Kang <75911206+BrianKang123@users.noreply.github.com>
2022-02-15 00:03:07 -0500

    Update factorial.java

--- cb/recursion-1/factorial.java
+++ cb/recursion-1/factorial.java
@@ -6,6 +6,14 @@ public int factorial(int n) {
 
 }
 
+public static void main(String[] args) {
+    System.out.print(factorial(1) + " → \"1");
+    System.out.print(factorial(2) + " → \"2");
+    System.out.print(factorial(3) + " → \"6");
+
+  }
+}
+
 //Not tree recursion
 //Only one line of calls and returns
 //A straight list of calls and return values of values multed to each other
